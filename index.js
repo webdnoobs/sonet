552f4002e83f0c29a88a488decb3b836678af82c
Prit Raj <prit.adra@gmail.com>
2017-08-17 07:58:45 +0530

    Config for serving static pages in production

--- index.js
+++ index.js
@@ -19,4 +19,12 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 require("./routes/authRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 app.listen(PORT);
